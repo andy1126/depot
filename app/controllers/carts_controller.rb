7de937096cd028d4d0b588025b4aa278d053e7e7
andy1126 <komnenos@outlook.com>
2014-09-16 20:02:26 -0400

    page 135

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
   def index
@@ -54,9 +54,18 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    if @cart.id == session[:cart_id]
+      @cart.destroy
+    else
+      respond_to do |format|
+        format.html{redirect_to store_url, notice: 'Don\'t get your hands on other guy\'s stuff'}
+        format.json { head :no_content }
+      end
+      return
+    end
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
@@ -71,4 +80,12 @@ class CartsController < ApplicationController
     def cart_params
       params[:cart]
     end
+
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    end
+
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ActiveRecord::Base
   has_many :line_items
 
   before_destroy :ensure_not_referenced_by_any_line_item
-
   validates :title, :image_url, :description, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -10,10 +9,13 @@ class Product < ActiveRecord::Base
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  after_validation :enclose_with_paragraph
 
   def self.latest
     Product.order(:updated_at).last
   end
+
+
 
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
@@ -22,6 +24,11 @@ class Product < ActiveRecord::Base
       errors.add(:base, 'Line Items present')
       return false
     end
+  end
+
+  def enclose_with_paragraph
+    tmp = "<p>" + self[:description].to_s + '<\p>'
+    self[:description] = tmp.to_sym
   end
 
 end
